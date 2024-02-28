7c4a6b5318772a165390ecf65d02f35c26c0b0c0
Hina Alvi <159819506+Hina457@users.noreply.github.com>
2024-02-28 02:43:30 -0800

    Add files via upload

---
+++ Question10.js
@@ -0,0 +1,11 @@
+//Exercise 10
+//My name is Hina Alvi
+//Dated: 22/feb/2024
+//This program will run just like code hello i am hina alvi
+console.log("Hello! I am Hina Alvi");
+//program2*/
+//Exercise 10
+//My name is Hina Alvi
+//Dated: 22/feb/2024
+//This program will be multiplication
+console.log(5 * 2);
---
+++ Question10.ts
@@ -0,0 +1,19 @@
+
+//Exercise 10
+//My name is Hina Alvi
+//Dated: 22/feb/2024
+
+//This program will run just like code hello i am hina alvi
+console.log("Hello! I am Hina Alvi");
+
+
+//program 2
+
+ //program2*/
+//Exercise 10
+//My name is Hina Alvi
+//Dated: 22/feb/2024
+
+//This program will be multiplication
+
+console.log(5*2);
