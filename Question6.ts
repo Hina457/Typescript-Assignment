e29bc6bad8ad3b4bedc46e0415a28602f507c5dc
Hina Alvi <159819506+Hina457@users.noreply.github.com>
2024-02-28 02:41:54 -0800

    Add files via upload

---
+++ Question6.ts
@@ -0,0 +1,7 @@
+
+
+
+let personname: string = `\n\t HINA\t\n`;
+console.log(personName);  
+let stripped: string = personName.trim();
+console.log(stripped);
---
+++ Question7.ts
@@ -0,0 +1,12 @@
+ 
+ 
+ //Addition//
+ console.log(5+3);
+
+ //subtraction
+ console.log(11-3);
+
+ //Multiplication
+ console.log(4*2);
+//Division
+ console.log(16 /2);
