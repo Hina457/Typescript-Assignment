78f72f0f1a493810215c00fbcef449c21752c908
Hina Alvi <159819506+Hina457@users.noreply.github.com>
2024-02-28 02:39:54 -0800

    Add files via upload

---
+++ Question3.ts
@@ -0,0 +1,9 @@
+//lower case
+let PersonName: string = "HINA";
+console.log("lowercase:", personName.toLowerCase ());
+
+//upper case
+console.log("uppercase:", personName.toLocaleUpperCase ());
+
+//Title case
+console.log("titlecase:", personName.replace(/\bw/g,c => c. toUpperCase ()));
