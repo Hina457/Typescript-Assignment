c57a86160cd9e15eebb406783e39a95ddea3c8f5
Hina Alvi <159819506+Hina457@users.noreply.github.com>
2024-03-02 23:04:09 -0800

    Add files via upload

---
+++ Question11.js
@@ -0,0 +1,4 @@
+var members = ['hina', 'duaa', 'maria', 'ahmed', 'junaid'];
+for (var i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ Question11.ts
@@ -0,0 +1,7 @@
+
+
+
+let Members: string[] =['hina','duaa','maria','ahmed','junaid'];
+for (let i=0; i<Members.length; i++){
+    console.log(Members[i]);
+}
