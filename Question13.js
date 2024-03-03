9c1e26e349393e43507cb1d9abe7ce63422586bd
Hina Alvi <159819506+Hina457@users.noreply.github.com>
2024-03-02 23:07:14 -0800

    Add files via upload

---
+++ Question13.js
@@ -0,0 +1,4 @@
+var transportation = ['civic', 'landcrusor', 'bike', 'bus', 'suzuki'];
+for (var i = 0; i < transportation.length; i++) {
+    console.log('I Would  like too own a  ' + transportation[i]);
+}
---
+++ Question13.ts
@@ -0,0 +1,5 @@
+
+let transportation : string [] =['civic','landcrusor','bike','bus','suzuki'];
+for (let i=0 ; i<transportation.length; i++){
+    console.log('I Would  like too own a  '+ transportation[i]);
+}
