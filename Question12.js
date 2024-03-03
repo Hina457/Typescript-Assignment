c4e976e1a0669f0cb89d8648a0c0a8dcd4a69e2d
Hina Alvi <159819506+Hina457@users.noreply.github.com>
2024-03-02 23:04:48 -0800

    Add files via upload

---
+++ Question12.js
@@ -0,0 +1,5 @@
+var members = ['hina', 'duaa', 'maria', 'ahmed', 'junaid'];
+var Message = 'kal class h governor house ma:';
+for (var i = 0; i < members.length; i++) {
+    console.log(Message + members[i]);
+}
---
+++ Question12.ts
@@ -0,0 +1,7 @@
+
+
+let members: string[] =['hina','duaa','maria','ahmed','junaid'];
+let Message: String='kal class h governor house ma:';
+for (let i=0; i<members.length; i++){
+    console.log(Message+members[i]);
+}
