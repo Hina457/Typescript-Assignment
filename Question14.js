8ecdccbd30bb914929c4bacc0c36191fb5ba7735
Hina Alvi <159819506+Hina457@users.noreply.github.com>
2024-03-18 07:53:34 -0700

    Add files via upload

---
+++ Question14.js
@@ -0,0 +1,4 @@
+var guest_list = ['Hina', 'Duaa', 'maria', 'junaid'];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected sir/Madem ' + guest_list[i] + ',\n we invited you on dinner tommorow.\n\nThank you');
+}
---
+++ Question14.ts
@@ -0,0 +1,7 @@
+
+
+
+let guest_list :string [] = ['Hina','Duaa','maria','junaid'];
+
+for (let i=0 ; i<guest_list.length; i++){
+console.log('Respected sir/Madem ' + guest_list [i] + ',\n we invited you on dinner tommorow.\n\nThank you')}
